16ef49a35e1e42fa0b341993459c44978713b339
Mohammed Basha <mohammedbasha9191@gmail.com>
2019-11-12 09:20:15 +0200

    Fetching the data from the news api

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -4,11 +4,26 @@ import NewSingle from './NewSingle';
 class News extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            news: []
+        }
+    }
+
+    componentDidMount() {
+        const URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=461d034d65f947bc8ba452f68fbde05d';
+
+        fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                news: data.articles
+            })
+        })
+        .catch(error => console.log(error));
     }
 
     renderItems = () => {
-        this.props.items.map(item => <NewSingle key={this.id} item={item} />);
+        return this.state.news.map(item => <NewSingle key={this.id} item={item} />);
     }
 
     render() { 
