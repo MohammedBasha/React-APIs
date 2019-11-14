84579ed0a5e85f971ff8f63b4893c26ad12dba85
Mohammed Basha <mohammedbasha9191@gmail.com>
2019-11-14 07:10:32 +0200

    Adding an Error component to handle the expected errors in the URL

---
+++ src/components/News/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <h3>
+            Opps, there is an error preventing the news feeding, please check it.
+        </h3>
+    )
+}
+
+export default Error;
--- src/components/News/SideNews.js
+++ src/components/News/SideNews.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
-import SingleSide from './SingleSide';
 import axios from 'axios';
+import SingleSide from './SingleSide';
+import Error from './Error';
 
 class SideNews extends Component {
   constructor(props) {
     super(props);
     this.state = {
       sidenews: [],
+      error: false
     };
   }
 
@@ -19,11 +21,18 @@ class SideNews extends Component {
         sidenews: response.data.articles
       });
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      this.setState({
+        error: true
+      });
+    });
   }
 
   renderItems() {
-    return this.state.sidenews.map(item => <SingleSide key={item.url} item={item} />);
+    if(!this.state.error)
+      return this.state.sidenews.map(item => <SingleSide key={item.url} item={item} />);
+    else
+      return <Error />
   }
 
   render() {
